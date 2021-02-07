fd0d0b3a15a8b29bc64030e8c6d4346a93fa6ecb
Gil Orcilla <mathnasiumcos@gmail.com>
2021-02-07 15:49:22 -0700

    provide full driver code

--- seeders/seed.js
+++ seeders/seed.js
@@ -13,6 +13,16 @@ mongoose.connect("mongodb://localhost/obscure-waters", {
   useFindAndModify: false,
 });
 
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://dbadmin:*.DaScie6914!$@cluster0.06lnd.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 let workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate() - 10),
