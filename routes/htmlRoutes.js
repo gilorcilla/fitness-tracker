d22258bf71275842d69585774a3b3912f879594c
Gil Orcilla <mathnasiumcos@gmail.com>
2021-02-11 20:10:40 -0700

    deploying

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,10 @@ const path = require("path");
 // router.get("/", function (req, res) {
 //   res.sendFile(path.join(__dirname, "../public/home.html"));
 //});
-router.get("/", function (req, res) {
+router.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-router.get("/", function (req, res) {
+router.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 module.exports = router;
