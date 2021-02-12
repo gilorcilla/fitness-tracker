297f9d91cea719473d43f4f5b6d3ff145a1d19ba
Gil Orcilla <mathnasiumcos@gmail.com>
2021-02-11 19:25:44 -0700

    modify files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+// router.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../public/home.html"));
+//});
 router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-router.use("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 router.get("/", function (req, res) {
